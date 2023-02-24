96ad2d2e3cb83092c61fea16c5ca853871740363
Joshua, Li <skyblocksians@gmail.com>
2023-02-24 22:26:08 +0800

    new savings

--- dbinit/init.sql
+++ dbinit/init.sql
@@ -4,11 +4,10 @@ ALTER DATABASE maindb DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 USE maindb;
 
-DROP TABLE IF EXISTS keywordcache;
-DROP TABLE IF EXISTS contentcache;
 DROP TABLE IF EXISTS keywords;
 DROP TABLE IF EXISTS contents;
 DROP TABLE IF EXISTS sentiments;
+DROP TABLE IF EXISTS cachekeywords;
 
 
 CREATE TABLE keywords (
@@ -36,21 +35,4 @@ CREATE TABLE sentiments (
   CONSTRAINT pk_sentiments PRIMARY KEY (sentimentsId),
   CONSTRAINT fk_sentimentscontents FOREIGN KEY (contentId)
   REFERENCES contents(contentId)
-);
-
-CREATE TABLE keywordcache (
-  cachekeywordId  INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  keyword         TEXT(2048) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-
-  CONSTRAINT pk_keywordcache PRIMARY KEY (cachekeywordId)
-);
-
-CREATE TABLE contentcache (
-  cachecontentId  INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  cachekeywordId  INT UNSIGNED NOT NULL,
-  content         TEXT(2048) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-
-  CONSTRAINT pk_contentcache PRIMARY KEY (cachecontentId),
-  CONSTRAINT fk_contentcachekeywords FOREIGN KEY (cachekeywordId)
-  REFERENCES keywords(cachekeywordId)
 );
